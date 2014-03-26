32de353aeb2b854ad72f8c814699ead958d3fe40
meriadec <meriadec.pillet@gmail.com>
2014-03-26 20:17:36 +0100

    so smoothy

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -1,17 +1,24 @@
 'use strict';
 
 angular.module('42StackApp')
-.controller('LoginCtrl', function ($scope, $http, $location, Flash, $cookies) {
+.controller('LoginCtrl', function ($scope, $http, $location, Flash, $cookies, $timeout) {
 
 	$scope.user = { name : '', mdp : '' };
+	$scope.logged = true;
+	$timeout(function () {
+		$scope.logged = false;
+	}, 500);
 
 	$scope.login = function() {
 		if ($scope.user.name != '' && $scope.user.mdp != '') {
 			$http.post('/authentificate', $scope.user)
 			.success(function (data, status, headers, config) {
 				$cookies.token = data.token;
+				$scope.logged = true;
 				Flash.set('Welcome to the awesome');
-				$location.url('/');
+				$timeout(function () {
+					$location.url('/');
+				}, 1000);
 			})
 			.error(function (data, status, headers, config) {
 				delete $cookies.token;
