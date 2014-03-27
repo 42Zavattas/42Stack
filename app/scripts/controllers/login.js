31753f24c4d062b7a3d9241fd85857d7242146bf
meriadec <meriadec.pillet@gmail.com>
2014-03-27 14:32:05 +0100

    authenticate with mongo

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -3,28 +3,28 @@
 angular.module('42StackApp')
 .controller('LoginCtrl', function ($scope, $http, $location, Flash, $cookies, $timeout) {
 
-	$scope.user = { name : '', mdp : '' };
+	$scope.user = { login : null, password : null };
 	$scope.uiLogged = true;
 	$timeout(function () {
 		$scope.uiLogged = false;
 	}, 500);
 
 	$scope.login = function() {
-		if ($scope.user.name != '' && $scope.user.mdp != '') {
+		if ($scope.user.login && $scope.user.password) {
 			$http.post('/authentificate', $scope.user)
 			.success(function (data, status, headers, config) {
 				$cookies.token = data.token;
 				$scope.$root.logged = true;
 				$scope.uiLogged = true;
-				Flash.set('Welcome to the awesome', 'success');
+				Flash.set('Welcome back, <strong>'+data.user.login+'</strong>', 'success');
 				$timeout(function () {
 					$location.url('/');
 				}, 500);
 			})
 			.error(function (data, status, headers, config) {
 				delete $cookies.token;
-				Flash.set('Authentification failed.', 'error');
-				$scope.user.mdp = '';
+				Flash.set(data, 'error');
+				$scope.user.password = null;
 			});
 		}
 	}
