44ea0e6f27079e5c532510efee00b195c64f2b54
meriadec <meriadec.pillet@gmail.com>
2014-03-27 13:39:29 +0100

    rearranging

--- app/scripts/controllers/questions.js
+++ app/scripts/controllers/questions.js
@@ -1,7 +1,21 @@
 'use strict';
 
 angular.module('42StackApp')
-.controller('QuestionsCtrl', function ($scope, questions, $routeParams) {
+.controller('QuestionsCtrl', function ($scope, questions, $routeParams, $location) {
 	$scope.questions = questions;
 	console.log($routeParams);
+
+	$scope.viewQuestion = function (question) {
+		$location.path('/questions/' + question._id);
+	};
+
+	$scope.viewCateg = function ($event, category) {
+		$event.stopPropagation();
+		$location.url('/questions?categ=' + category);
+	}
+
+	$scope.viewTag = function ($event, tag) {
+		$event.stopPropagation();
+		$location.url('/questions?tags=' + tag);
+	}
 });
