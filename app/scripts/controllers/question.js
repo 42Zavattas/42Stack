4ef702246102082c532029955e259c8101a95bdc
meriadec <meriadec.pillet@gmail.com>
2014-03-27 23:59:58 +0100

    posting answer : the beggining

--- app/scripts/controllers/question.js
+++ app/scripts/controllers/question.js
@@ -1,10 +1,31 @@
 'use strict';
 
-angular.module('42StackApp').controller('QuestionCtrl', function ($http, $scope, question, $location) {
+angular.module('42StackApp').controller('QuestionCtrl', function ($http, $scope, question, $location, Flash) {
 
 	$scope.question = question;
+	$scope.question.answers = [];
+	$scope.answer = resetAnswer();
 
 	$scope.viewTag = function (tag) {
 		$location.url('/questions?tags=' + tag);
 	};
+
+	$scope.postAnswer = function () {
+		if (!$scope.answer.msg) {
+			Flash.set('<strong>Invalid purpose !</strong> Type an answer before posting ?', 'error');
+		}
+		if ($scope.answer.msg) {
+			$scope.question.answers.push({
+				msg : $scope.answer.msg
+			});
+		}
+	};
+
+	function resetAnswer () {
+		$scope.answer = {
+			msg : null
+		};
+		return $scope.answer;
+	}
+
 });
