5703dfa1e7b2eea6101f284d47f8a31fb1483b71
Aperçu <bgronon@gmail.com>
2014-03-27 17:35:07 +0100

    testing signup

--- app/scripts/controllers/signup.js
+++ app/scripts/controllers/signup.js
@@ -1,10 +1,24 @@
 'use strict';
 
-angular.module('42StackApp').controller('SignupCtrl', function ($scope, $http) {
+angular.module('42StackApp')
+.controller('SignupCtrl', function ($scope, $http, $location, Flash) {
 
 	$scope.login = '';
 
 	$scope.send = function () {
-		
+		if ($scope.login) {
+			console.log("pute");
+
+			$http.post('/signup', $scope.login)
+			.success(function (data, status, headers, config) {
+				Flash.set('Welcome to 42Stack <strong>' + $scope.login + '</strong> !', 'success');
+				console.log("oooo");
+				$location.url('/login');
+			})
+			.error(function (data, status, headers, config) {
+				$scope.login = '';
+				Flash.set('Error processing your login', 'error');
+			});
+		}
 	}
 });
