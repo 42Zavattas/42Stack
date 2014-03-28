fffc3f7cc6e1981d3f180e186463e1b64758c873
meriadec <meriadec.pillet@gmail.com>
2014-03-28 14:00:01 +0100

    fix

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -11,11 +11,16 @@ angular.module('42StackApp').controller('MainCtrl', function ($scope, $location,
 	$scope.viewCateg = function ($event, category) {
 		$event.stopPropagation();
 		$location.url('/questions?categ=' + category);
-	}
+	};
 
 	$scope.viewTag = function ($event, tag) {
 		$event.stopPropagation();
 		$location.url('/questions?tags=' + tag);
-	}
+	};
+
+	$scope.viewUser = function ($event, user) {
+		$event.stopPropagation();
+		$location.url('/users/' + user._id);
+	};
 
 });
