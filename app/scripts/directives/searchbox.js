844f561303fcc85c5e2c3f0d0185f09acc026dd0
meriadec <meriadec.pillet@gmail.com>
2014-03-29 00:14:36 +0100

    better searchbox

--- app/scripts/directives/searchbox.js
+++ app/scripts/directives/searchbox.js
@@ -1,26 +1,40 @@
 'use strict';
 
 angular.module('42StackApp')
-.directive('searchbox', function () {
+.directive('searchbox', function ($timeout) {
 	return {
 		templateUrl: 'directives/searchbox.html',
 		restrict: 'E',
 		link: function postLink(scope, element, attrs) {
 			scope.searchText = null;
 			scope.searchActive = false;
+			scope.pinned = false;
 			scope.searchGlobal = function (text) {
 				console.log(text);
 				scope.searchText = null;
 				scope.searchActive = false;
 			};
+			scope.togglePin = function () {
+				scope.pinned = !scope.pinned;
+				scope.active = scope.pinned;
+				if (scope.active) {
+					element.find('input').focus();;
+				}
+			};
 			scope.toggleActive = function () {
 				scope.searchActive = !scope.searchActive;
+				$timeout(function () { element.find('input').focus(); }, 50);
 			};
 			scope.setActive = function () {
-				scope.searchActive = true;
+				if (!scope.pinned) {
+					scope.searchActive = true;
+					$timeout(function () { element.find('input').focus(); }, 50);
+				}
 			};
 			scope.setInactive = function () {
-				scope.searchActive = false;
+				if (!scope.pinned) {
+					scope.searchActive = false;
+				}
 			};
 		}
 	};
