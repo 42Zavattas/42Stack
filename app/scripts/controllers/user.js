81726e8ba6a13f943a9696101f2bfcafae9b1786
Balthazar GRONON <bgronon@gmail.com>
2014-03-29 12:21:41 +0100

    work

--- app/scripts/controllers/user.js
+++ app/scripts/controllers/user.js
@@ -8,14 +8,46 @@ angular.module('42StackApp')
 	$scope.chart = {
 		options : {
 			chart : {
-				type : 'line',
-				zoomType : 'x'
+				type : 'spline',
+				zoomType : 'x',
+				backgroundColor: 'rgba(0, 0, 0, 0.1)',
+				borderRadius: 0,
+				spacingTop: 20
+			},
+			title : {
+				text : ''
+			},
+			credits : {
+				enabled : false
+			},
+			xAxis: {
+				type: 'datetime',
+				labels: {
+					overflow: 'justify'
+				},
+				offset: 0,
+				lineColor: "rgba(0, 0, 0, 0.1)"
+			},
+			yAxis: {
+				title: {
+					text: ''
+				},
+				tickInterval: 10,
+				min: 0,
+				minorGridLineWidth: 0,
+				gridLineWidth: 0,
+				alternateGridColor: null
+			},
+			legend : {
+				borderWidth: 0,
+				itemHoverStyle: {
+					color: ''
+				}
 			}
 		},
 		series : [{
 			name : 'Reputation changes',
 			data : [1, 2, 3, 4, 8, 42, 24, 16, 8]
-		}],
-		xAxis: {currentMin: 0, currentMax: 10, minRange: 1}
+		}]
 	};
 });
