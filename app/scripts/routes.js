251a8e30d04d621c9d2c4d964b154778d2550715
Balthazar Gronon <bgronon@gmail.com>
2014-05-01 18:27:44 +0200

    remove useless

--- app/scripts/routes.js
+++ app/scripts/routes.js
@@ -56,20 +56,6 @@ angular.module('42StackApp')
 					Restangular.all('users').getList()
 				]).then(function (res) {
 					data.users = res[0];
-					angular.forEach(data.users, function (user) {
-						Restangular.all('votes').getList({ toUser : user._id }).then(function (res){
-							angular.forEach(res, function(vote) {
-								if (vote.type === -1) {
-									user.reputation -= 2;
-								}
-								else if (vote.type === 1) {
-									user.reputation += (vote.objtype === 'answer') ? 10 : 5;
-								}
-							});
-						}, function (err) {
-							deferred.reject(err);
-						});
-					});
 					deferred.resolve(data);
 				}, function (err) {
 					deferred.reject(err);
@@ -137,31 +123,11 @@ angular.module('42StackApp')
 							deferred.reject('question '+$route.current.params.id+' not found');
 						}
 						question.author = data.users[question.author];
-						angular.forEach(data.votes, function (vote) {
-							if (vote.obj === question._id) {
-								if (vote.type === -1) {
-									question.downvotes++;
-								}
-								else if (vote.type === 1) {
-									question.upvotes++;
-								}
-							}
-						});
 						return question;
 					})(res[0]);
 					data.answers = (function (answers) {
 						angular.forEach(answers, function (el) {
 							el.author = data.users[el.author];
-							angular.forEach(data.votes, function (vote) {
-								if (vote.obj === el._id) {
-									if (vote.type === -1) {
-										el.downvotes++;
-									}
-									else if (vote.type === 1) {
-										el.upvotes++;
-									}
-								}
-							});
 						});
 						return answers;
 					})(res[2]);
@@ -190,16 +156,6 @@ angular.module('42StackApp')
 					data.votes = indexify(res[2]);
 					angular.forEach(data.questions, function (question) {
 						question.author = data.users[question.author];
-						angular.forEach(data.votes, function (vote) {
-							if (vote.obj === question._id) {
-								if (vote.type === -1) {
-									question.downvotes++;
-								}
-								else if (vote.type === 1) {
-									question.upvotes++;
-								}
-							}
-						});
 					});
 					deferred.resolve(data);
 				}, function (err) {
